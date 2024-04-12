9d585c5a387407b12b92edda61a4f65f04eb1d81
Medina Mustafa <medinaamustafa@gmail.com>
2024-04-12 22:52:06 +0200

    Added some models

---
+++ BookReviewApp.Server/Controllers/Entities/Rating.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookReviewApp.Server.Controllers.Entities
+{
+    public class Rating
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        public string? Comment { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+
+        // Navigation 
+        public int BookId { get; set; }
+        public Book? Book { get; set; }
+
+
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ BookReviewApp.Server/Controllers/Entities/UserBook.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookReviewApp.Server.Controllers.Entities
+{
+    public class UserBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        //Navigation
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public int BookId { get; set; }
+        public Book? Book { get; set; }
+    }
+}
