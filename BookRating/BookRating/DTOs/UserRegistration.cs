08a33b29474900864aa838ca87ba0e601e7f82f5
Grinesaa <nasernuredini8@gmail.com>
2024-04-16 23:41:48 +0200

    Refactor user authentication flow

--- BookRating/BookRating/DTOs/UserRegistration.cs
+++ BookRating/BookRating/DTOs/UserRegistration.cs
@@ -1,8 +1,19 @@
-﻿namespace BookTating.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookTating.DTOs;
 
 public class UserRegistration
 {
+    [Required(ErrorMessage = "Username is required.")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 30 characters.")]
     public required string Username { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address.")]
     public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).*$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
     public required string Password { get; set; }
 }
