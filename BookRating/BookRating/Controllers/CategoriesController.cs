db2cbb0f3bb8a1198749d6134e01281ed16e754b
Medina Mustafa <medinaamustafa@gmail.com>
2024-04-21 01:22:58 +0200

    .

--- BookRating/BookRating/Controllers/CategoriesController.cs
+++ BookRating/BookRating/Controllers/CategoriesController.cs
@@ -1,11 +1,13 @@
 ﻿using BookRating.DataAccess;
 using BookRating.DTOs.Category;
 using BookRating.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookRating.Controllers
 {
     
+    public class CategoriesController 
+    {
+    }
 }
