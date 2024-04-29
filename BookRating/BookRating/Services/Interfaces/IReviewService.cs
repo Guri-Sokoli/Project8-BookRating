ae7a2f4c658cc72a3ca58685240709d121342ba7
Grinesaa <nasernuredini8@gmail.com>
2024-04-29 11:22:20 +0200

    .

--- BookRating/BookRating/Services/Interfaces/IReviewService.cs
+++ BookRating/BookRating/Services/Interfaces/IReviewService.cs
@@ -7,7 +7,7 @@ namespace BookRating.Services.Interfaces
     {
         Task<Review> CreateReview(int userId, int bookId, ReviewDto reviewDto);
         Task<Review> EditReview(int userId, int reviewId, ModifiedReviewDto reviewDto);
-        Task<IEnumerable<ReviewResponseDto>> GetUserReviews(int userId);
+        Task<IEnumerable<Review>> GetUserReviews(int userId);
         Task<object> GetReview(int reviewId);
         Task<IEnumerable<Review>> GetAllReviews();
         Task DeleteReview(int userId, int reviewId);
