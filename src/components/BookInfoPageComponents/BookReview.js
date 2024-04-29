159787be2c1336c2f7042537a3cf5437d071fba0
OLT11GAMER <93716411+OLT11GAMER@users.noreply.github.com>
2024-04-29 10:32:53 +0200

    renaming

---
+++ src/components/BookInfoPageComponents/BookReview.js
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+
+const BookReview = ({ review }) => {
+    const renderStars = (rating) => {
+        let stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= Math.floor(rating)) {
+                stars.push(renderStar(i, "full"));
+            } else if (i - 0.5 <= rating) {
+                stars.push(renderStar(i, "half"));
+            } else {
+                stars.push(renderStar(i, "empty"));
+            }
+        }
+        return stars;
+    };
+
+    const renderStar = (i, type) => {
+        let fill;
+        if (type === "full") {
+            fill = "#FCB500";
+        } else if (type === "half") {
+            fill = "url(#half-gradient)";
+        } else {
+            fill = "#BAB6AE";
+        }
+
+        return (
+            <svg
+                key={i}
+                xmlns="http://www.w3.org/2000/svg"
+                fill={fill}
+                className={`peer peer-hover:fill-[${
+                    false ? "#FCB500" : "#BAB6AE"
+                }] hover:fill-[${
+                    false ? "#FCB500" : "#BAB6AE"
+                }] w-8 h-8 md:w-10 md:h-10 mx-1 cursor-${
+                    false ? "pointer" : "default"
+                }`}
+                viewBox="0 0 24 24"
+                stroke="none"
+                aria-label={`Star ${i}`}
+            >
+                <defs>
+                    <linearGradient id="half-gradient">
+                        <stop offset="50%" stop-color="#FCB500" />
+                        <stop offset="50%" stop-color="#BAB6AE" />
+                    </linearGradient>
+                </defs>
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 2l3.09 6.26L22 9.27l-5 4.73L18.18 22 12 18.7 5.82 22l1.18-7.04L2 9.27l6.91-1.01L12 2z"
+                />
+            </svg>
+        );
+    };
+    return (
+        <div>
+            <div className="flex flex-col bg-[#F9F1DE] md:flex-row justify-center md:justify-around items-center my-8">
+                <div className="flex flex-row md:flex-col px-12">
+                    <div className="flex flex-row justify-center items-center">
+                        <img
+                            className="rounded-full w-16 h-16 bg-gray-300 m-4 md:w-32 md:h-32"
+                            src={review.pfp ? review.pfp : "/user.png"}
+                            alt="User Picture"
+                        />
+                        <h1>{review.userName}</h1>
+                    </div>
+                    <div className="flex flex-row justify-center items-center mb-4">
+                        {renderStars(review.rating)}
+                    </div>
+                </div>
+                <h1 className="px-12 text-wrap w-full text-[#59461B] text-md font-medium sm:text-lg md:text-xl">
+                    {review.comment}
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default BookReview;
---
+++ src/components/Footer.js
@@ -0,0 +1,74 @@
+import React from "react";
+import facebookIcon from "../assets/facebook.svg";
+import twitterIcon from "../assets/twitter.svg";
+import instagramIcon from "../assets/instagram.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+
+const Footer = () => {
+    return (
+        <div className="flex flex-row justify-around items-start bg-[#59461B] text-white px-6 py-6 md:py-10">
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-center text-lg pb-4 md:pb-10 sm:text-2xl md:text-3xl">
+                    Company
+                </h1>
+                <ul className="text-center text-sm sm:text-lg md:text-xl">
+                    <li className="my-2">About Us</li>
+                    <li className="my-2">Careers</li>
+                    <li className="my-2">Terms</li>
+                    <li className="my-2">Privacy</li>
+                    <li className="my-2">Help</li>
+                </ul>
+            </div>
+            <div className="flex flex-col justify-center items-center mx-4">
+                <h1 className="text-center text-lg pb-4 md:pb-10 sm:text-2xl md:text-3xl">
+                    Work With Us
+                </h1>
+                <ul className="text-center text-sm sm:text-lg md:text-xl">
+                    <li className="my-2">Authors</li>
+                    <li className="my-2">Advertise</li>
+                    <li className="my-2">Authors & Blogs</li>
+                    <li className="my-2">API</li>
+                </ul>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-center text-lg pb-4 md:pb-10 sm:text-2xl md:text-3xl">
+                    Connect
+                </h1>
+                <ul className="flex flex-row justify-between">
+                    <li className="mx-0.5 sm:mx-2">
+                        <a href="https://www.facebook.com">
+                            <img
+                                src={facebookIcon}
+                                alt="Facebook"
+                                className="h-6"
+                            />
+                        </a>
+                    </li>
+                    <li className="mx-0.5 sm:mx-2">
+                        <img src={twitterIcon} alt="Twitter" className="h-6" />
+                    </li>
+                    <li className="mx-0.5 sm:mx-2">
+                        <a href="https://www.instagram.com">
+                            <img
+                                src={instagramIcon}
+                                alt="Instagram"
+                                className="h-6"
+                            />
+                        </a>
+                    </li>
+                    <li className="mx-0.5 sm:mx-2">
+                        <a href="https://www.linkedin.com">
+                            <img
+                                src={linkedinIcon}
+                                alt="LinkedIn"
+                                className="h-6"
+                            />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
